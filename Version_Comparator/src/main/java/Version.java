27ee6a6d8a02e9579764bb6ad191e6a9fa8af353
Nauris <nauris.gruduls@gmail.com>
2024-05-25 12:04:58 +0300

    add version implementation

--- Version_Comparator/src/main/java/Version.java
+++ Version_Comparator/src/main/java/Version.java
@@ -11,7 +11,24 @@ public class Version implements Comparable<Version> {
     private final boolean isSnapshot;
 
     public Version(String version) {
+        if (version == null) {
+            throw new IllegalArgumentException("'version' must not be null!");
+        }
 
+        String regex = "\\d+(\\.\\d+){0,2}(-SNAPSHOT)?";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(version);
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("'version' must match: 'major.minor.patch(-SNAPSHOT)'!");
+        }
+
+        this.version = version;
+        String[] parts = version.split("-")[0].split("\\.");
+        this.major = Integer.parseInt(parts[0]);
+        this.minor = parts.length > 1 ? Integer.parseInt(parts[1]) : 0;
+        this.patch = parts.length > 2 ? Integer.parseInt(parts[2]) : 0;
+        this.isSnapshot = version.endsWith("-SNAPSHOT");
     }
 
     public boolean isSnapshot() {
@@ -20,7 +37,27 @@ public class Version implements Comparable<Version> {
 
     @Override
     public int compareTo(Version other) {
-        return 0;
+        if (other == null) {
+            throw new IllegalArgumentException("'other' must not be null!");
+        }
+
+        if (this.isSnapshot && !other.isSnapshot) {
+            return -1;
+        }
+        if (!this.isSnapshot && other.isSnapshot) {
+            return 1;
+        }
+
+        if (this.major != other.major) {
+            return Integer.compare(this.major, other.major);
+        }
+        if (this.minor != other.minor) {
+            return Integer.compare(this.minor, other.minor);
+        }
+        return Integer.compare(this.patch, other.patch);
     }
 
+    public static void main(String[] args) {
+        Version version1 = new Version("0.0.1");
+    }
 }
